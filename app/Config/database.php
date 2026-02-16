cefdbf3f089a176b745a719df0e0fe8b56f0d704
kefla-yup <itielarandria@gmail.com>
2026-02-16 22:56:49 +0300

    avec htacces

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'host' => '127.0.0.1',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'bngrc_dons',
+    'host' => '172.16.7.108',
+    'username' => 'ETU003888',
+    'password' => 'bFr05fcc',
+    'database' => 'db_s2_ETU003888',
     'charset' => 'utf8mb4'
 ];
 ?>
